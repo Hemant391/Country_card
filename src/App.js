215e2d1d83e919083d39c5bf2711deeff9939e85
Hemant391 <148564538+Hemant391@users.noreply.github.com>
2024-06-14 10:22:44 +0530

    searchbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import Countries from "./Country";
+import React, { useState } from 'react'
+import './app.css'
 function App() {
+  const [name,setName]=useState('')
  
+  const handlesearch=(e)=>{
+    setName(e.target.value)
+  }
   return (
     <div className="Container" >
-      <Countries />
+      <div className="searchbar">
+        <input type="text" value={name} onChange={handlesearch} style={{width:'50%',padding:'5px'}} placeholder="Search for countries.."/>
+      </div>
+      <Countries Countryname={name} />
     </div>
   );
 }
--- src/Country.jsx
+++ src/Country.jsx
@@ -1,6 +1,6 @@
 import React, { useState,useEffect } from 'react'
     function Carddetail({name,image,alt}){
-        return(<div className='card' style={{width:'200px',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center', width:"190px",border:'1px solid black',
+        return(<div className='countryCard' style={{width:'200px',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center', width:"190px",border:'1px solid black',
         padding:'10px',margin:'10px',height:'200px',borderRadius:'8px',
     }}>
         <img style={{width:'100%'}} src={image} alt={alt} />
@@ -8,27 +8,43 @@ import React, { useState,useEffect } from 'react'
     </div>)
     }
 
-export default function Country(){
+export default function Country({Countryname}){
+    const [filter,setFilter]=useState([])
+   
     let url='https://restcountries.com/v3.1/all'
 
     const [detail,setDetail]=useState([]);
+   
     const fetchdata=async ()=>{
         try{
             let response=await fetch(url);
             let data= await response.json();
-           
-            setDetail(data);
+            console.log(data)
+            let filterdata=[]
+            if(Countryname!=''){
+
+          filterdata= data.filter((e)=>{
+                   return( e.name.common.includes(Countryname))
+                })
+                setDetail(filterdata);
+            }else{
+
+                setDetail(data);
+            }
 
         }catch(e){
             console.log('Error: ',e);
         }
     }
     useEffect(()=>{
-        fetchdata();
-    },[])
+        console.log(Countryname)
+        fetchdata()
+        
+        
+    },[Countryname])
     
     return (
-        <div style={{display:'flex',flexWrap:'wrap',alignItems:'center',height:'100vh'}}>
+        <div style={{display:'flex',flexWrap:'wrap',height:'100vh',paddingTop:'15px',justifyContent:'center'}}>
 
             {detail.map((data)=>(<Carddetail key={data.name.common}  name={data.name.common} image={data.flags.png} alt={data.flags.alt} />))}
         </div>
